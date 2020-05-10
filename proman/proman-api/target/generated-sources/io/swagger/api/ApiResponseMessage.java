8a69e2f8459273eb1989c9fb7781148457381bb6
husainasad <shusain_be16@thapar.edu>
2020-05-11 01:40:43 +0530

    created controller class and service module

--- proman/proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
+++ proman/proman-api/target/generated-sources/io/swagger/api/ApiResponseMessage.java
@@ -2,7 +2,7 @@ package io.swagger.api;
 
 import javax.xml.bind.annotation.XmlTransient;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-09T03:49:57.929+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-11T01:33:17.770+05:30")
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
--- proman/proman-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ proman/proman-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-09T03:49:57.929+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-11T01:33:17.770+05:30")
 
 @Configuration
 public class SwaggerDocumentationConfig {
